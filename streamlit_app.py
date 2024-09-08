21ee7689ebb4b23271bc248a039a963a21b587f9
Mrunmayeed <mrunmayee.dhapre.98@gmail.com>
2024-09-08 03:16:52 -0700

    Added links and cleaned code

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 st.set_page_config(layout='wide')
 
-col1,col2 = st.columns([2,3])
+
 
 class ResumeBot:
 #Load the models
@@ -44,7 +44,6 @@ class ResumeBot:
         #Configure Chroma as a retriever with top_k=1
         self.retriever = vectordb.as_retriever(search_kwargs={"k": 5})\
 
-#Create the retrieval chain
 
     def create_chain(self):
         template = """
@@ -65,8 +64,7 @@ class ResumeBot:
         prompt = PromptTemplate.from_template(template)
         combine_docs_chain = create_stuff_documents_chain(self.llm, prompt)
         self.retrieval_chain = create_retrieval_chain(self.retriever, combine_docs_chain)
-
-    #Invoke the retrieval chain
+        st.session_state['chain'] = self.retrieval_chain
 
     def testing(self):
 
@@ -78,71 +76,53 @@ class ResumeBot:
             response= self.retrieval_chain.invoke({"input":i})
             print(response["answer"])
 
-
     def generate_response(self, text):
         response = self.retrieval_chain.invoke({"input":text})
 
         return response['answer'], response['context']
 
+    def education(self, tab):
+        ms = tab.button('M. S. Computer Science')
+        if ms:
+            tab.markdown(""" 
+            - San Jose State University
+            - Courses: Big Data, Machine Learning, Cloud Computing, Graph Neural Network   
+            """)
 
-    def setup(self):
+        bs = tab.button('B. Tech. Computer Engineering')
+        if bs:
+            tab.markdown(""" 
+            - VJTI (Mumbai University)
+            - Courses: Database Management, Operating Systems, Data Mining, Blockchain   
+            """)
 
-        self.load_pdf()
-        self.create_chain()
-
-        col3,col4 = col1.columns([5,6])
-
-        col3.image("sq_dp.jpg", width=200)
-
-        col4.title('Mrunmayee Dhapre')
-        col4.subheader('M.S. Computer Science')
-        # col4.
-
-        tab1, tab2, tab3 = col1.tabs(['Experience','Skills','Projects'])
-
-        # tab1.markdown(""" 
-        #               ##### Data Engineer @ Fractal AI (2020-2023)
-        #                 - Led the backend team developing ETL pipelines and REST APIs
-        #                 - Warehousing solution with event-driven microservices architecture
-
-        #               ##### Data Engineer Intern @ LendingClub (Summer 2024)
-        #                 - LLMs for categorization of customer chat data in Snowflake
-        #                 - Customizable app for clustering and chi-square analysis in Streamlit
-                      
-        #               ##### Research Assistant @ SJSU (2023 - present)
-        #                 - Time Series Forecasting on groundwater logs with LSTM, Transformer
-        #                 - Real Time Machine Learning pipeline with Airflow
-                      
-        #                """)
-        
-        de_frac = tab1.button('Data Engineer @ Fractal AI (2020-2023)')
+    def workex(self, tab):
+        de_frac = tab.button('Data Engineer @ Fractal AI (2020-2023)')
         if de_frac:
-            tab1.markdown(""" 
+            tab.markdown(""" 
                     - Led the backend team developing ETL pipelines and REST APIs
                     - Warehousing solution with event-driven microservices architecture""")
             
         
-        de_lc = tab1.button('Data Engineer Intern @ LendingClub (Summer 2024)')
+        de_lc = tab.button('Data Engineer Intern @ LendingClub (Summer 2024)')
         if de_lc:
-            tab1.markdown(""" 
+            tab.markdown(""" 
                     - LLMs for categorization of customer chat data in Snowflake
                     - Customizable app for clustering and chi-square analysis in Streamlit""")
             
         
-        ra = tab1.button('Research Assistant @ SJSU (2023 - present)')
+        ra = tab.button('Research Assistant @ SJSU (2023 - present)')
         if ra:
-            tab1.markdown(""" 
+            tab.markdown(""" 
                     - Time Series Forecasting on groundwater logs with LSTM, Transformer
                     - Real Time Machine Learning pipeline with Airflow""")
+            
+    def skills(self, tab):
+        col1, col2 = tab.columns([2,2])
 
-        
-        # tab2.markdown('### Skills:')
-
-        col5, col6 = tab2.columns([2,2])
-
-        lang = col5.button('Languages')
+        lang = col1.button('Languages')
         if lang:
-            col6.markdown("""
+            col2.markdown("""
             - Python
             - SQL
             - Java
@@ -151,9 +131,9 @@ class ResumeBot:
             """)
 
 
-        webdev = col5.button('Web Development')
+        webdev = col1.button('Web Development')
         if webdev:
-            col6.markdown("""
+            col2.markdown("""
                         - Streamlit
                         - Flask
                         - FastAPI
@@ -161,9 +141,9 @@ class ResumeBot:
                         - Angular
                         """)
             
-        etl = col5.button('ETL & BigData')
+        etl = col1.button('ETL & BigData')
         if etl:
-            col6.markdown("""
+            col2.markdown("""
                         - Airflow
                         - Snowflake
                         - Spark
@@ -172,9 +152,9 @@ class ResumeBot:
                         - Knime
                         """)
             
-        db = col5.button('Database Management')
+        db = col1.button('Database Management')
         if db:
-            col6.markdown("""
+            col2.markdown("""
                         - MSSQL
                         - MySQL
                         - Neo4J
@@ -183,9 +163,9 @@ class ResumeBot:
                         - Oracle
                         """)
             
-        azure = col5.button('Cloud Platforms')
+        azure = col1.button('Cloud Platforms')
         if azure:
-            col6.markdown("""
+            col2.markdown("""
                         - Kubernetes
                         - ServiceBus
                         - FunctionApp
@@ -194,9 +174,9 @@ class ResumeBot:
                         - Managed Workflows for Apache Airflow
                         """)
             
-        ai = col5.button('AI/ML Modules')
+        ai = col1.button('AI/ML Modules')
         if ai:
-            col6.markdown("""
+            col2.markdown("""
                         - Keras
                         - Tensorflow
                         - Pandas
@@ -206,28 +186,70 @@ class ResumeBot:
                         - llamaindex
                         - openai-whisper
                         """)
-
-        nlp = tab3.button('Sentiment Analysis for Movie Review')
+            
+    def projects(self, tab):
+        nlp = tab.button('Sentiment Analysis for Movie Review')
         if nlp:
-            tab3.markdown("""
+            tab.markdown("""
             - Natural Language Processing in Spark
             - Detected fake reviews on a web scraped dataset
             """)
 
-        ot = tab3.button('Predictive Modeling for Operational Technology Application')
+        ot = tab.button('Predictive Modeling for Operational Technology Application')
         if ot:
-            tab3.markdown("""
+            tab.markdown("""
             - Time Series Forecasting on Industrial Control System logs
             - Hybrid LSTM and GRU models for anomaly detection
             - Published in  International Journal for Artificial Intelligence, 2021
             """)
 
-        rag = tab3.button('Book Recommendation Chatbot')
+        rag = tab.button('Book Recommendation Chatbot')
         if rag:
-            tab3.markdown("""
+            tab.markdown("""
             - Chatbot recommendating books by descriptions and genres
             - RAG chatbot implemented with LangChain and HuggingFace
             """)
+
+    def links(self, col):
+        # log1,log2,log3 = col1.columns([2,2,2])
+        # col1.markdown(
+        # '''
+        #     [![Title](https://badgen.net/badge/icon/linkedin?color=orange&icon=https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg&label)](https://in.linkedin.com/in/mrunmayee-dhapre) 
+        #     [![Title](https://badgen.net/badge/icon/GitHub?color=orange&icon=github&label)](https://github.com/Mrunmayeed) 
+        #     [![Title](https://badgen.net/badge/icon/medium?color=orange&icon=medium&label)](https://medium.com/@mrunmayee.dhapre)
+        # ''')
+        col.markdown(
+        '''
+            [![Title](https://badgen.net/badge/icon/GitHub?color=orange&icon=github&label)](https://github.com/Mrunmayeed) 
+        ''')
+
+        col.markdown(
+        '''
+            [![Title](https://badgen.net/badge/icon/medium?color=orange&icon=medium&label)](https://medium.com/@mrunmayee.dhapre)
+        ''')
+
+        # st.logo('github-logo.png',link='https://github.com/Mrunmayeed')
+        st.logo('linkedin-logo.png',link='https://in.linkedin.com/in/mrunmayee-dhapre')
+    
+    def sidepane(self, col1):
+        col3,col4 = col1.columns([5,6])
+
+        col3.image("dp_photo.png", width=200)
+
+        col4.title('Mrunmayee Dhapre')
+        # col4.subheader('M.S. Computer Science')
+        self.links(col4)
+
+        tab1, tab2, tab3, tab4 = col1.tabs(['Education','Experience','Skills','Projects'])
+        self.education(tab1)
+        
+        self.workex(tab2)
+
+        self.skills(tab3)
+
+        self.projects(tab4)
+        
+        # tab2.markdown('### Skills:')
 
         css = '''
         <style>
@@ -239,14 +261,7 @@ class ResumeBot:
 
         col1.markdown(css, unsafe_allow_html=True)
 
-        # col1.markdown(""" 
-        #               ##### Research Assistant @ SJSU (2023 - present)
-
-        #               ##### Data Engineer Intern @ LendingClub (Summer 2024)
-                      
-        #               ##### Data Engineer @ Fractal AI (2020-2023)
-        #               """)
-
+    def showchats(self, col2):
         messages = col2.container(height=600)
 
         if 'messages' not in st.session_state:
@@ -257,14 +272,27 @@ class ResumeBot:
 
         if prompt := col2.chat_input("Ask a question about me"):
 
-            messages.chat_message("user").write(prompt)
+            messages.chat_message("ai").write(prompt)
             response,context = self.generate_response(prompt)
-            messages.chat_message("ai").write(response)
+            messages.chat_message("user").write(response)
             print(f"Context for {prompt}:{context}")
 
 
-            st.session_state.messages.append({"role": "user", "content": prompt})
-            st.session_state.messages.append({"role": "ai", "content": response})
+            st.session_state.messages.append({"role": "ai", "content": prompt})
+            st.session_state.messages.append({"role": "user", "content": response})
+
+    def setup(self):
+
+        if 'chain' in st.session_state:
+            self.retrieval_chain=st.session_state['chain']
+        else:
+            self.load_pdf()
+            self.create_chain()
+
+
+        col1,col2 = st.columns([2,3])
+        self.sidepane(col1)
+        self.showchats(col2)        
 
 
 rb = ResumeBot()
